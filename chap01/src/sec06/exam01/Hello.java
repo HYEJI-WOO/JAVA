d38505b573e4b3bd8839044729214fbde090d4cd
s <s@USERMIC-RBHBTFG>
2022-09-30 16:09:46 +0900

    Example

---
+++ chap01/src/sec06/exam01/Hello.java
@@ -0,0 +1,16 @@
+package sec06.exam01;
+
+
+/*작성자 : 우혜지
+작성일시 : 2022년 09월 26일
+*/
+
+public class Hello { // 클래스 블록 시작 
+	
+	// 메인메소드 : 프로그램 시작점 , 이름은 반드시 main
+	public static void main(String[] args) { // 메인메소드 블록 시작
+		System.out.println("Hello");
+		System.out.println("World"); 
+	} // 메인메소드 블록 끝 
+
+} // 클래스 블록 끝
