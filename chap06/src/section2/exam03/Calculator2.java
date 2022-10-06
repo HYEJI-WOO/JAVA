c2cc99ba68efd49ce9fb880a6225fb5f3741304e
HYEJI-WOO <whj1939@naver.com>
2022-10-06 15:15:03 +0900

    add

---
+++ chap06/src/section2/exam03/Calculator2.java
@@ -0,0 +1,15 @@
+package section2.exam03;
+
+public class Calculator2 {
+	
+	// 정사각형
+	int areaRectangle(int width) {
+		return width*width;
+	}
+	
+	// 직사각형
+	int areaRectangle(int width, int height) {
+		return width*height;
+	}
+	
+}
