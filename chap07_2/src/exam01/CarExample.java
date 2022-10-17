7cb6b8219073a5c4fff87d330bf1602bb03279ed
HYEJI-WOO <whj1939@naver.com>
2022-10-17 10:09:07 +0900

    add

--- chap07_2/src/exam01/CarExample.java
+++ chap07_2/src/exam01/CarExample.java
@@ -5,25 +5,13 @@ public class CarExample {
 		Car car = new Car();
 		
 		for (int i = 0; i < 5; i++) {
+			// 펑크가 난 타이어 숫자 반환
 			int problemLocation = car.run();
-			
-			switch (problemLocation) {
-			case 1:
-				System.out.println("앞왼쪽 한국타이어로 교체");
-				car.frontLeftTire = new HankookTire("앞왼쪽", 10);
-				break;
-			case 2:
-				System.out.println("앞오른쪽 한국타이어로 교체");
-				car.frontRightTire = new HankookTire("앞오른쪽", 10);
-				break;
-			case 3:
-				System.out.println("뒤왼쪽 금호타이어로 교체");
-				car.backLeftTire = new KumhoTire("뒤왼쪽", 10);
-				break;
-			case 4:
-				System.out.println("뒤오른쪽 한국타이어로 교체");
-				car.backRightTire = new HankookTire("뒤오른쪽", 10);
-				break;
+			if(problemLocation!=0) {
+				System.out.println(
+					car.tires[problemLocation-1].location + " 한국타이어로 교체");
+				car.tires[problemLocation-1] =
+					new HankookTire(car.tires[problemLocation-1].location, 10);
 			}
 			System.out.println("===================");
 		}
