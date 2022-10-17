f3f332e0acc46c229647077b123b5ec2ee113bdd
HYEJI-WOO <whj1939@naver.com>
2022-10-17 09:50:18 +0900

    add

---
+++ chap07_2/src/exam01/CarExample.java
@@ -0,0 +1,32 @@
+package exam01;
+
+public class CarExample {
+	public static void main(String[] args) {
+		Car car = new Car();
+		
+		for (int i = 0; i < 5; i++) {
+			int problemLocation = car.run();
+			
+			switch (problemLocation) {
+			case 1:
+				System.out.println("앞왼쪽 한국타이어로 교체");
+				car.frontLeftTire = new HankookTire("앞왼쪽", 10);
+				break;
+			case 2:
+				System.out.println("앞오른쪽 한국타이어로 교체");
+				car.frontRightTire = new HankookTire("앞오른쪽", 10);
+				break;
+			case 3:
+				System.out.println("뒤왼쪽 금호타이어로 교체");
+				car.backLeftTire = new KumhoTire("뒤왼쪽", 10);
+				break;
+			case 4:
+				System.out.println("뒤오른쪽 한국타이어로 교체");
+				car.backRightTire = new HankookTire("뒤오른쪽", 10);
+				break;
+			}
+			System.out.println("===================");
+		}
+	}
+
+}
