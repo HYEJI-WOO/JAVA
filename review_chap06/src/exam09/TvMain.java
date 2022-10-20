4b3e2e7926eb3a087f07e1c4c5f8ded80e2f8706
HYEJI-WOO <whj1939@naver.com>
2022-10-20 11:10:08 +0900

    add

--- review_chap06/src/exam09/TvMain.java
+++ review_chap06/src/exam09/TvMain.java
@@ -3,9 +3,15 @@ package exam09;
 public class TvMain {
 	public static void main(String[] args) {
 		System.out.println(Television.info);
-//		Television television = new Television();
 		System.out.println(Television.info);
 		
+		System.out.println("==================");
+		Television tv1 = new Television("º¸¶ó");
+		tv1.methodA();
+		
+		Television tv2 = new Television("³ë¶û");
+		tv2.methodA();
+	
 	}
 
 }
