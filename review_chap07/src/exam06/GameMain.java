5008b68022a2f7a99501eb3deb7a2abd7245ca95
HYEJI-WOO <whj1939@naver.com>
2022-10-21 11:48:54 +0900

    add

---
+++ review_chap07/src/exam06/GameMain.java
@@ -0,0 +1,14 @@
+package exam06;
+
+public class GameMain {
+	public static void main(String[] args) {
+		Hero hero = new Hero("包快");
+		Sword s1 = new Sword("公扁1");
+		Sword s2 = new Sword("公扁2");
+		Spear p1 = new Spear("芒1");
+		Bow b1 = new Bow("劝1");
+		hero.setItem(b1);
+		hero.info();
+	}
+
+}
---
+++ review_chap07/src/exam06/Item.java
@@ -0,0 +1,7 @@
+package exam06;
+
+public abstract class Item {
+	
+	public abstract String getItem();
+
+}
