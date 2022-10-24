8dfe59609a988bfa340bc480c230a158bafad73d
HYEJI-WOO <whj1939@naver.com>
2022-10-24 10:15:11 +0900

    add

---
+++ review_chap08/src/exam01/Dummy.java
@@ -0,0 +1,14 @@
+package exam01;
+
+public abstract class Dummy implements RemoteControl {
+	
+	public abstract void dummyMethod();
+	
+
+	@Override
+	public void turnOff() {
+		System.out.println("TV ²û");
+	}
+	
+
+}
---
+++ review_chap08/src/exam03/Building.java
@@ -0,0 +1,12 @@
+package exam03;
+
+public abstract class Building implements 
+			Liftable, Readable, Generatable{
+}
+
+class DummyA{}
+class DummyB{}
+
+interface Liftable{}
+interface Repareable{}
+interface Generatable{}
