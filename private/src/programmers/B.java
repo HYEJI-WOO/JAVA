6c77375207e4ad155097daa542fdd2d44d6ab2ce
HYEJI-WOO <whj1939@naver.com>
2022-10-31 09:20:40 +0900

    add

--- private/src/programmers/B.java
+++ private/src/programmers/B.java
@@ -4,7 +4,16 @@ public class B {
 	public static void main(String[] args) {
 		String before = "olleh";
 		String after = "hello";
-		String result = "";
+		int result = 0;
+		
+		for(int i=0; i<before.length(); i++) {
+			for(int j=0; j<after.length(); j++) {
+				if(before.charAt(i)==after.charAt(j)) {
+					result ++;
+				}
+			}
+		}
+		System.out.println(result);
 		
 
 	}
